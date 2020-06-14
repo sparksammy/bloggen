98436d0386ebdb0bb50ab658312a2c42edb7bf4a
Samuel Lord <sllord@icloud.com>
2020-06-13 22:05:04 -0500

    The beginning...
    Starting off basic with: A Python3.8 script, a test post, and a launcher. :-)

---
+++ bloggenpublic.py
@@ -0,0 +1,28 @@
+#Sparksammy's Python3 Static Blog Generator
+import glob
+blogname = "Your First Blog" #Replace me, then put the posts in the posts/ folder as .txt files.
+adcode = '' #Add your AD code here, perferibly with an hr tag preceding it.
+copyrightnotice = '<p>Copyright YOURNAMEHERE - All rights reserved.</p>' #Add your copyright notice here.
+#Ignore everyting else here unless you know what you are doing.
+postsfolder = glob.glob("posts/*.txt")
+blog = open("blog.html", "w+")
+blog.write('<!DOCTYPE html>')
+blog.write('<html>')
+blog.write('<head><title>' + blogname + '</title></head>')
+blog.write('<body>')
+blog.write('<h1>' + blogname + '</h1><hr><a href="index.html">Go home</a> <a href="old-blogs.html">Historic posts</a>')
+
+for filename in postsfolder:
+    og = open(filename, "r")
+    ogt = og.readline()
+    ogc = og.read().replace('\n','').replace(ogt,'')
+    blog.write('<hr/>')
+    blog.write('<h2>' + ogt + '</h2>')
+    blog.write('<p>' + ogc + '</p>')
+    blog.write(adcode);
+    og.close()
+blog.write('<hr/>')
+blog.write(copyrightnotice)
+blog.write('</body>')
+blog.write('</html>')
+blog.close()
---
+++ launcherpublic.bat
@@ -0,0 +1,7 @@
+@echo off
+color 0a
+title Blog Generator
+echo Generating...
+C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python38\python.exe bloggenpublic.py
+echo Done!
+pause
